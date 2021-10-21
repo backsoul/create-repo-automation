439f428572a655f8c4fa99bf06b45922a5b683dd
DanielSarmientoDev <danielsarmientodeveloper@gmail.com>
2021-10-21 12:23:42 -0500

    fix: change file

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const excmd = (cmd) => {
     return execSync(cmd, { encoding: 'utf-8' })
 }
 
-const folders = excmd('cd repositories && ls').split("\n")
+const folders = excmd('cd repositories && ls').split("\n").slice(0, -1)
 
-const create_repo = (name_repo) => {
+/* const create_repo = (name_repo) => {
     const name = name_repo
     return new Promise(async(resolve, reject) => {
         const body = { name };
@@ -26,21 +26,26 @@ const create_repo = (name_repo) => {
             resolve(data)
         }).catch(err => reject(err))
     })
-}
+} */
 
-const user = process.env.USER_GITHUB
+// const user = process.env.USER_GITHUB
 
-const create_repo_local = (name_repo) => {
+/* const create_repo_local = (name_repo) => {
     const cmd = `cd repositories/${name_repo} && git init && git add . && git commit -m "initial commit" && git remote add origin git@github.com:${user}/${name_repo} && git push origin master`
     excmd(cmd);
     return `carpeta inicializada: ${name_repo}`
-}
+} */
 
+
+const initial_repo = (folder) => {
+    const cmd_pull = `cd repositories/${folder} && git init && git remote add origin git@github.com:${process.env.NAME_REPO}/${folder} && git pull origin master`
+    excmd(cmd_pull);
+    //const cmd_push = `cd repositories/${folder} && git add . && git commit -m "Actualización Shf addons a SHF ODOO" && git push origin master`
+
+}
+excmd("cd repositories/a && rm -rf .git");
+excmd("cd repositories/b && rm -rf .git");
+excmd("cd repositories/c && rm -rf .git");
 for (const folder of folders) {
-    create_repo(folder).then((_) => {
-        console.log('====================================');
-        console.log(`se creo correctamente el repositorio en github: (${folder})`);
-        console.log('====================================');
-        create_repo_local(folder);
-    }).catch((err) => console.log("error al crear carpeta en github..", err));
+    initial_repo(folder)
 }
